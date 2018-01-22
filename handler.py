72f3bb3a02b55a67eb25a1a9de07aafa1e66fbba
Philipp Keller <philipp.keller@gmail.com>
2018-01-22 20:39:16 +0100

    bugfix, also upload to dest bucket

--- handler.py
+++ handler.py
@@ -1,8 +1,12 @@
 import os
 def handler(event, context):
 	import ocr
-	bucket = os.environ['S3_BUCKET']
-	file = event['file']
+	src_bucket = os.environ['S3_SRC_BUCKET']
+	dest_bucket = os.environ['S3_DEST_BUCKET']
+	src_file = event['file']
+	dest_file = src_file.split()[0] + '.pdf'
+
 	# S3_BUCKET = 'scanner-upload'
 	# S3_FILE = 'scan_2018-01-20_155119.tar.gz'
-	ocr.ocr(bucket, file, "")
+
+	ocr.ocr(src_bucket, src_file, dest_bucket, dest_file)

bbb9b756062a84f5ac442fb7df9274f94e3c305a
Philipp Keller <philipp.keller@gmail.com>
2018-01-29 21:13:38 +0100

    add removing of empty pages

--- ocr.py
+++ ocr.py
@@ -2,7 +2,7 @@ import boto3
 import tarfile
 import os
 import subprocess
-import pdfrw
+from pdfrw import PdfReader, PdfWriter
 
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -17,21 +17,29 @@ s3 = boto3.client('s3')
 # - try tessxeract 4.0..?
 # - use tesseract_fast to lower the size (4MB vs 40MB!)
 # - try if w/o deu package it makes any difference..
-def ocr(src_bucketname, src_filename, dest_bucketname, dest_filename):
-	print("pdfrw version: {}".format(pdfrw.__version__))
-	if src_bucketname == dest_bucketname:
-		raise Exception('cannot store resulting pdf in same bucket as source files as it would result in an endless loop')
-	s3.download_file(src_bucketname, src_filename, "{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
-	tar = tarfile.open("{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
-	tar.extractall(path=TMP_DIR)
-	with open('{}/filenames.txt'.format(TMP_DIR), 'w') as f:
-	    f.write("\n".join(["{}/{}".format(TMP_DIR, n) for n in tar.getnames()]))
-	env = os.environ.copy()
-	env.update(dict(LD_LIBRARY_PATH=LIB_DIR, TESSDATA_PREFIX=SCRIPT_DIR))
-	out = subprocess.check_output(['./tesseract', 
-		'{}/filenames.txt'.format(TMP_DIR),
-		'{}/out'.format(TMP_DIR),
-		'pdf'], cwd=SCRIPT_DIR, env=env)
-	s3.upload_file("{}/out.pdf".format(TMP_DIR), dest_bucketname, dest_filename)
-	for f in ['out.pdf', 'filenames.txt', DOWNLOAD_FILE] + tar.getnames():
-		os.remove("{}/{}".format(TMP_DIR, f))
+def ocr(src_bucketname, src_filename, dest_bucketname, dest_filename, empty_page_threshold):
+    if src_bucketname == dest_bucketname:
+        raise Exception('cannot store resulting pdf in same bucket as source files as it would result in an endless loop')
+    s3.download_file(src_bucketname, src_filename, "{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
+    tar = tarfile.open("{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
+    tar.extractall(path=TMP_DIR)
+    env = os.environ.copy()
+    env.update(dict(LD_LIBRARY_PATH=LIB_DIR, TESSDATA_PREFIX=SCRIPT_DIR))
+
+    output = PdfWriter()
+    for filename in tar.getnames():
+        out = subprocess.check_output(['./tesseract', 
+            '{}/{}'.format(TMP_DIR, filename),
+            '{}/partial'.format(TMP_DIR),
+            'pdf'], cwd=SCRIPT_DIR, env=env)
+        for p in PdfReader("{}/{}".format(TMP_DIR, "partial.pdf")).pages:
+            try:
+                if int(p.Contents['/Length']) > empty_page_threshold:
+                    output.addpage(p)
+            except:
+                # e.g. Contents is None, or Contents as no key '/Length' etc
+                pass
+    output.write('{}/output.pdf'.format(TMP_DIR))
+    s3.upload_file("{}/output.pdf".format(TMP_DIR), dest_bucketname, dest_filename)
+    for f in ['partial.pdf', 'output.pdf', DOWNLOAD_FILE] + tar.getnames():
+        os.remove("{}/{}".format(TMP_DIR, f))

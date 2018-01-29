32973ba5380e5bf9c0524178a061ccb0ea703805
Philipp Keller <philipp.keller@gmail.com>
2018-01-29 21:13:38 +0100

    add removing of empty pages

--- handler.py
+++ handler.py
@@ -1,9 +1,10 @@
 import os
 def handler(event, context):
-	dest_bucket = os.environ['S3_DEST_BUCKET']
-	import ocr
-	for record in event['Records']:
-		src_bucket = record['s3']['bucket']['name']
-		src_file = record['s3']['object']['key']
-		dest_file = src_file.split('.')[0] + '.pdf'
-		ocr.ocr(src_bucket, src_file, dest_bucket, dest_file)
+    import ocr
+    dest_bucket = os.environ['S3_DEST_BUCKET']
+    empty_page_threshold = int(os.environ['EMPTY_PAGE_THRESHOLD'])
+    for record in event['Records']:
+        src_bucket = record['s3']['bucket']['name']
+        src_file = record['s3']['object']['key']
+        dest_file = src_file.split('.')[0] + '.pdf'
+        ocr.ocr(src_bucket, src_file, dest_bucket, dest_file, empty_page_threshold)

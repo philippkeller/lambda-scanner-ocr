b9233d9e68b378dce377186c4f678523d7448625
Philipp Keller <philipp.keller@gmail.com>
2018-01-29 06:45:47 +0100

    going for pdfrw for detecting empty pages

--- ocr.py
+++ ocr.py
@@ -2,6 +2,7 @@ import boto3
 import tarfile
 import os
 import subprocess
+import pdfrw
 
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,7 +12,15 @@ TMP_DIR = '/tmp'
 
 s3 = boto3.client('s3')
 
+# todo: 
+# - remove empty pages using http://www.binpress.com/tutorial/pdfrw-the-other-python-PDF-library/171
+# - try tessxeract 4.0..?
+# - use tesseract_fast to lower the size (4MB vs 40MB!)
+# - try if w/o deu package it makes any difference..
 def ocr(src_bucketname, src_filename, dest_bucketname, dest_filename):
+	print("pdfrw version: {}".format(pdfrw.__version__))
+	if src_bucketname == dest_bucketname:
+		raise Exception('cannot store resulting pdf in same bucket as source files as it would result in an endless loop')
 	s3.download_file(src_bucketname, src_filename, "{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
 	tar = tarfile.open("{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
 	tar.extractall(path=TMP_DIR)
@@ -23,7 +32,6 @@ def ocr(src_bucketname, src_filename, dest_bucketname, dest_filename):
 		'{}/filenames.txt'.format(TMP_DIR),
 		'{}/out'.format(TMP_DIR),
 		'pdf'], cwd=SCRIPT_DIR, env=env)
-	print(out)
 	s3.upload_file("{}/out.pdf".format(TMP_DIR), dest_bucketname, dest_filename)
 	for f in ['out.pdf', 'filenames.txt', DOWNLOAD_FILE] + tar.getnames():
 		os.remove("{}/{}".format(TMP_DIR, f))

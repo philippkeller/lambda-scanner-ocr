3db751e56797986d06cc4979df049f5b78494cf7
Philipp Keller <philipp.keller@gmail.com>
2018-02-06 17:32:40 +0100

    start working on getting gdrive upload to work

--- handler.py
+++ handler.py
@@ -1,14 +1,69 @@
 import os
 def handler(event, context):
+    """
+    download tar.gz, do ocr and upload it to configured cloud service
+    (currently AWS S3 or Google Drive)
+    """
     import ocr
-    dest_bucket = os.environ['S3_DEST_BUCKET']
-    empty_page_threshold = int(os.environ['EMPTY_PAGE_THRESHOLD'])
-    if 'TESSERACT_LANG' in os.environ:
-    	language = os.environ['TESSERACT_LANG']
+    
+    if os.environ['UPLOAD_TYPE'] == 'gdrive':
+        upload_type = 'gdrive'
+        folder = os.environ.get('GDRIVE_FOLDER', None)
+        client_id = os.environ['GDRIVE_CLIENT_ID']
+        client_secret =  os.environ['GDRIVE_CLIENT_SECRET']
+        refresh_token = os.environ['GDRIVE_REFRESH_TOKEN']
+        upload_params = (client_id, client_secret, refresh_token, folder)
+    elif os.environ['UPLOAD_TYPE'] == 's3':
+        bucket = os.environ['S3_BUCKET']
+        upload_params = (bucket)
     else:
-    	language = 'eng'
+        raise Exception('unknown upload type {}'.format(os.environ['UPLOAD_TYPE']))
+
+    empty_page_threshold = int(os.environ.get('EMPTY_PAGE_THRESHOLD', 200))
+    language = os.environ.get('TESSERACT_LANG', 'eng')
+
+    import boto3
+    s3 = boto3.client('s3')
     for record in event['Records']:
         src_bucket = record['s3']['bucket']['name']
         src_file = record['s3']['object']['key']
-        dest_file = src_file.split('.')[0] + '.pdf'
-        ocr.ocr(src_bucket, src_file, dest_bucket, dest_file, empty_page_threshold, language)
+
+        s3.download_file(src_bucket, src_file, "/tmp/scan.tar.gz")
+        pdf_file = ocr.ocr("/tmp/scan.tar.gz", empty_page_threshold, language)
+
+        dest_filename = src_file.split('.')[0] + '.pdf'
+        if upload_type == 's3':
+            s3.upload_file(pdf_file, upload_params[0], dest_filename)
+        elif upload_type == 'gdrive':
+            upload_gdrive(pdf_file, dest_filename, *upload_params)
+
+        
+def upload_gdrive(file_src, file_dest, client_id, client_secret, refresh_token, folder=None):
+    import httplib2
+    import os
+    from apiclient import discovery
+    from oauth2client import client
+    from apiclient.http import MediaFileUpload
+
+    credentials = client.GoogleCredentials(None, 
+        client_id, 
+        client_secret,
+        refresh_token,
+        None,
+        "https://accounts.google.com/o/oauth2/token",
+        'scanner-ocr')
+
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    file_metadata = {
+        'name': file_dest,
+    }
+    if folder is not None:
+        file_metadata['parents'] = [folder]
+
+    media = MediaFileUpload(file_src,
+                            mimetype='image/jpeg')
+    file = service.files().create(body=file_metadata,
+                                  media_body=media,
+                                  fields='id').execute()
--- ocr.py
+++ ocr.py
@@ -4,7 +4,6 @@ import os, sys
 import subprocess
 from pdfrw import PdfReader, PdfWriter
 
-
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.join(SCRIPT_DIR, 'lib')
 DOWNLOAD_FILE = 'scan.tar.gz'
@@ -17,11 +16,8 @@ s3 = boto3.client('s3')
 # - try tessxeract 4.0..?
 # - use tesseract_fast to lower the size (4MB vs 40MB!)
 # - try if w/o deu package it makes any difference..
-def ocr(src_bucketname, src_filename, dest_bucketname, dest_filename, empty_page_threshold, language='eng'):
-    if src_bucketname == dest_bucketname:
-        raise Exception('cannot store resulting pdf in same bucket as source files as it would result in an endless loop')
-    s3.download_file(src_bucketname, src_filename, "{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
-    tar = tarfile.open("{}/{}".format(TMP_DIR, DOWNLOAD_FILE))
+def ocr(tar_gz_filename, empty_page_threshold, language='eng'):
+    tar = tarfile.open(tar_gz_filename)
     tar.extractall(path=TMP_DIR)
     env = os.environ.copy()
     env.update(dict(LD_LIBRARY_PATH=LIB_DIR, TESSDATA_PREFIX="{}/tessdata".format(SCRIPT_DIR)))
@@ -48,7 +44,8 @@ def ocr(src_bucketname, src_filename, dest_bucketname, dest_filename, empty_page
                 pass
             output.addpage(p)
     output.write('{}/output.pdf'.format(TMP_DIR))
-    s3.upload_file("{}/output.pdf".format(TMP_DIR), dest_bucketname, dest_filename)
+
     s3.delete_object(Bucket=src_bucketname, Key=src_filename)
-    for f in ['partial.pdf', 'output.pdf', DOWNLOAD_FILE] + tar.getnames():
+    for f in ['partial.pdf', DOWNLOAD_FILE] + tar.getnames():
         os.remove("{}/{}".format(TMP_DIR, f))
+    return '{}/output.pdf'.format(TMP_DIR)
